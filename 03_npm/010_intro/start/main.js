ec231284b5b6aa50d58a5300a24c25652be88fe3
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-14 14:39:25 +0900

    35. パッケージが読み込まれる流れを学ぼう

--- 03_npm/010_intro/start/main.js
+++ 03_npm/010_intro/start/main.js
@@ -1,9 +1,10 @@
-import _ from "lodash";
+// import _ from "lodash";
+const cloneDeep = require("lodash/cloneDeep");
 
 const original = { prop: { nested: "value" } };
 
 // オブジェクトの複製
-const cloned = _.cloneDeep(original);
+const cloned = cloneDeep(original);
 cloned.prop.nested = "new value";
 
 console.log(original, cloned);
