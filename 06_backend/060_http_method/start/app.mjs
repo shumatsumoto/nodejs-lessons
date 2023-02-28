8160a3dd44edcbd81d9c421326508cade2a287e4
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-02-28 20:23:19 +0900

    92. HTTPメソッドについて学ぼう１

--- 06_backend/060_http_method/start/app.mjs
+++ 06_backend/060_http_method/start/app.mjs
@@ -1,12 +1,12 @@
-import * as http from 'http';
+import * as http from "http";
 
 /**
  * リクエストメソッド
- * 
+ *
  * GETメソッド: コンテンツの取得
  * 　　　タイミング：ブラウザのURL指定、リンクのクリック、<form>のデフォルト
  *      特徴：URLの一部にパラメータを渡す
- * 
+ *
  * POSTメソッド: コンテンツの作成
  * 　　　 タイミング：<form>のPOSTメソッド
  *       特徴：リクエストの本文にパラメータを渡す
@@ -14,10 +14,16 @@ import * as http from 'http';
 
 const server = http.createServer(function (req, res) {
   console.log(req.url);
-  if (req.url === '/hello') {
-    res.end(`<h1>こんにちは</h1>`);
-  } else if (req.url === '/bye') {
-    res.end('bye');
+  if (req.url === "/") {
+    res.write(`<a href="/result">Get Method Link</a>`);
+    res.end(`
+      <form action="/result" method="POST">
+        <input type="text" name="title">
+        <input type="submit">
+      </form>
+    `);
+  } else if (req.url === "/bye") {
+    res.end("bye");
   } else {
     res.end(req.url);
   }
