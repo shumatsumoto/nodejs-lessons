5da07a9e1021bfdbb06d9e8221eb997e580f0d4a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-03-01 15:07:49 +0900

    94. HTTPメソッドについて学ぼう３

--- 06_backend/060_http_method/start/app.mjs
+++ 06_backend/060_http_method/start/app.mjs
@@ -18,14 +18,24 @@ const server = http.createServer(function (req, res) {
     res.end(`
       <form action="/result" method="POST">
         <input type="text" name="title">
+        <input type="text" name="description">
         <input type="submit">
       </form>
     `);
   } else {
     console.log(req.url, req.method);
     if (req.method === "GET") {
-      const aaa = new URLSearchParams(req.url);
-      console.log(aaa);
+      const getParams = new URLSearchParams(req.url);
+      console.log(getParams);
+    } else if (req.method === "POST") {
+      let data = "";
+      req.on("data", (chunk) => {
+        data += chunk;
+      });
+      req.on("end", function () {
+        const postParams = new URLSearchParams(data);
+        console.log(postParams);
+      });
     }
     res.end(req.url);
   }
