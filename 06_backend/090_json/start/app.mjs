3ffd49db1f97e426925d8d04cff8ffd397965bfc
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-03-02 12:49:25 +0900

    100. JSONを返却する実装を行ってみよう

--- 06_backend/090_json/start/app.mjs
+++ 06_backend/090_json/start/app.mjs
@@ -1,13 +1,13 @@
-import * as http from 'http';
-import express from 'express';
+import * as http from "http";
+import express from "express";
 
 const PORT = 8080;
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('hello');
-})
+app.get("/", function (req, res) {
+  res.send({ message: "hello", number: 3, array: ["banana", 3, 34] });
+});
 
-app.listen(PORT, function() {
-    console.log(`Server start: http://localhost:${PORT}`)
+app.listen(PORT, function () {
+  console.log(`Server start: http://localhost:${PORT}`);
 });
