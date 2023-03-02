cbb938cfb10252e94bb20cb5d5a6962620ec91c2
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-03-02 12:38:15 +0900

    99. 【Express】練習問題

--- 06_backend/080_express_practice/start/app.mjs
+++ 06_backend/080_express_practice/start/app.mjs
@@ -1,9 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const PORT = 8080;
 const app = express();
 
-app.get('/', function (req, res) {
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
   res.send(`
     <h1>練習問題</h1>
     <p>下記のフォームで送信したリクエストを「商品1、商品2がカートに追加されました。」のようなレスポンスとして返却する処理を記述しましょう</p>
@@ -19,3 +21,11 @@ app.get('/', function (req, res) {
     `);
 });
 
+app.post("/cart", function (req, res) {
+  const products = req.body.product;
+  res.send(`${products.join(", ")}がカートに追加されました。`);
+});
+
+app.listen(PORT, function () {
+  console.log(`Server start http://localhost:${PORT}`);
+});
